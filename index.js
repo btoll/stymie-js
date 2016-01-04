6c773564f433fbf1a7483eb1194fb4d05baa618e
Benjamin Toll <benjam72@yahoo.com>
2016-01-04 07:35:44 -0500

    Decided to move all u/p into one file
    There is now another library that will do all
    the operations on files. The older functionality
    will still work, but it will be for notes, etc
    and not to store the u/p.
    
    The files will still be stored in their own files
    in ~/.stymie.d/s like before.

--- index.js
+++ index.js
@@ -9,65 +9,268 @@
         fs = require('fs'),
         inquirer = require('inquirer'),
         jcrypt = require('jcrypt'),
+        libFile = require('./lib/file'),
         util = require('./lib/util'),
         logError = util.logError,
         logInfo = util.logInfo,
         logSuccess = util.logSuccess,
         env = process.env,
-        stymieDir = `${env.STYMIE || env.HOME}/.stymie.d`,
-        entryDir = `${stymieDir}/s`,
+        listFile = `${env.STYMIE || env.HOME}/.stymie.d/l`,
         Stymie;
 
-    Stymie = {
-        add: (() => {
-            let newEntry, password, hashedFilename;
+    function generatePassphrase(entry, username, password) {
+        console.log(password = diceware.generate());
 
-            function formatData(username, password) {
-                return `${newEntry}\n\t${username}\n\t${password}`;
-            }
-
-            function generatePassphrase(username) {
-                console.log(password = diceware.generate());
-
-                inquirer.prompt([{
-                    type: 'list',
-                    name: 'password',
-                    message: 'Accept?:',
-                    choices: [
-                        {name: 'Yes', value: true},
-                        {name: 'No, generate another', value: false}
-                    ]
-                }], (answers) => {
-                    if (answers.password) {
-                        makeFileEntry(username, password);
-                    } else {
-                        generatePassphrase(username);
-                    }
-                });
-            }
-
-            function makeFileEntry(username, password) {
-                let listFile = `${stymieDir}/l`;
-
-                jcrypt.stream(formatData(username, password), `${entryDir}/${hashedFilename}`, {
-                    gpg: util.getGPGArgs(),
-                    file: {
-                        flags: 'w',
-                        defaultEncoding: 'utf8',
-                        fd: null,
-                        mode: 0o0600
-                    }
-                }, true)
+        inquirer.prompt([{
+            type: 'list',
+            name: 'password',
+            message: 'Accept?:',
+            choices: [
+                {name: 'Yes', value: true},
+                {name: 'No, generate another', value: false}
+            ]
+        }], (answers) => {
+            if (answers.password) {
+                makeListEntry(entry, username, password)
                 .then(() => {
-                    logSuccess('Entry created');
-
-                    return jcrypt(listFile, null, ['--decrypt'], true);
+                    logSuccess('Created new entry');
                 })
-                .then((data) => {
-                    let list = new Set(JSON.parse(data));
-                    list.add(newEntry);
+                .catch(logError);
+            } else {
+                generatePassphrase(entry, username, password);
+            }
+        });
+    }
 
-                    return jcrypt.stream(JSON.stringify(Array.from(list), null, 4), `${stymieDir}/l`, {
+    function makeListEntry(entry, username, password) {
+        return jcrypt(listFile, null, ['--decrypt'], true)
+        .then((data) => {
+            let list = JSON.parse(data);
+
+            list[entry] = {
+                username: username,
+                password: password
+            };
+
+            return jcrypt.stream(JSON.stringify(list, null, 4), listFile, {
+                gpg: util.getGPGArgs(),
+                file: {
+                    flags: 'w',
+                    defaultEncoding: 'utf8',
+                    fd: null,
+                    mode: 0o0600
+                }
+            }, true)
+        });
+    }
+
+    Stymie = {
+        add: (entry, onFile) => {
+            if (!entry) {
+                logError('No entry name');
+                return;
+            }
+
+            if (!onFile) {
+                jcrypt(listFile, null, ['--decrypt'], true)
+                .then((data) => {
+                    let list = JSON.parse(data);
+
+                    if (list[entry]) {
+                        logInfo('Entry already exists');
+                    } else {
+                        inquirer.prompt([{
+                            type: 'input',
+                            name: 'username',
+                            message: 'Enter username:',
+                            validate: util.noBlanks
+                        }, {
+                            type: 'list',
+                            name: 'generatePassword',
+                            message: 'Generate diceware password?',
+                            default: false,
+                            choices: [
+                                {name: 'Yes', value: true},
+                                {name: 'No', value: false}
+                            ]
+                        }, {
+                            type: 'password',
+                            name: 'password',
+                            message: 'Enter password:',
+                            validate: util.noBlanks,
+                            when: (answers) => {
+                                return !answers.generatePassword;
+                            }
+                        }], (answers) => {
+                            if (answers.generatePassword) {
+                                generatePassphrase(entry, answers.username, answers.password);
+                            } else {
+                                makeListEntry(entry, answers.username, answers.password)
+                                .then(() => {
+                                    logSuccess('Created new entry');
+                                })
+                                .catch(logError);
+                            }
+                        });
+                    }
+                })
+                .catch(logError);
+            } else {
+                libFile.add(entry);
+            }
+        },
+
+        edit: (entry, onFile) => {
+            if (!onFile) {
+                jcrypt(listFile, null, ['--decrypt'], true)
+                .then((data) => {
+                    let list = JSON.parse(data),
+                        item = list[entry];
+
+                    if (item) {
+                        inquirer.prompt([{
+                            type: 'input',
+                            name: 'key',
+                            message: 'Edit key:',
+                            default: entry,
+                            validate: util.noDuplicates.bind(null, list)
+                        }, {
+                            type: 'input',
+                            name: 'username',
+                            message: 'Edit username:',
+                            default: item.username
+                        }, {
+                            type: 'input',
+                            name: 'password',
+                            message: 'Edit password:',
+                            default: item.password
+                        }], (answers) => {
+                            let key = answers.key,
+                                username = answers.username,
+                                password = answers.password;
+
+                            if (key !== entry) {
+                                // Remove old entry.
+                                delete list[entry];
+
+                                item = list[key] = {};
+
+                                // Note if the key has changed the condition
+                                // below will always pass.
+                            }
+
+                            if (username !== item.username || password !== item.password) {
+                                item.username = username;
+                                item.password = password;
+
+                                jcrypt.stream(JSON.stringify(list, null, 4), listFile, {
+                                    gpg: util.getGPGArgs(),
+                                    file: {
+                                        flags: 'w',
+                                        defaultEncoding: 'utf8',
+                                        fd: null,
+                                        mode: 0o0600
+                                    }
+                                }, true)
+                                .then(() => {
+                                    logSuccess('Entry has been updated');
+                                })
+                                .catch(logError);
+                            } else {
+                                logInfo('No change');
+                            }
+                        });
+                    } else {
+                        logInfo('No matching entry');
+                    }
+                })
+                .catch(logError);
+            } else {
+                libFile.edit(entry);
+            }
+        },
+
+        generate: () => {
+            console.log(diceware.generate());
+        },
+
+        get: (entry, onFile) => {
+            if (!onFile) {
+                jcrypt(listFile, null, ['--decrypt'], true)
+                .then((data) => {
+                    let list = JSON.parse(data),
+                        item = list[entry];
+
+                    if (item) {
+                        console.log(`username = ${item.username}, password = ${item.password}`);
+                    } else {
+                        logInfo('No matching entry');
+                    }
+                }).catch(logError);
+            } else {
+                libFile.get(entry);
+            }
+        },
+
+        has: (entry, onFile) => {
+            if (!onFile) {
+                jcrypt(listFile, null, ['--decrypt'], true)
+                .then((data) => {
+                    logInfo(
+                        JSON.parse(data)[entry] ?
+                            'Entry exists' :
+                            'No matching entry'
+                    );
+                }).catch(logError);
+            } else {
+                libFile.has(entry);
+            }
+        },
+
+        list: (onFile) => {
+            if (!onFile) {
+                jcrypt(listFile, null, ['--decrypt'], true)
+                .then((data) => {
+                    logInfo('Installed keys:', Object.keys(JSON.parse(data)).sort().join(', '));
+                });
+            } else {
+                // TODO
+                logInfo('Not implemented');
+            }
+        },
+
+        remove: (entry, onFile) => {
+            if (!onFile) {
+                jcrypt(listFile, null, ['--decrypt'], true)
+                .then((data) => {
+                    let list = JSON.parse(data);
+
+                    return new Promise((resolve, reject) => {
+                        if (list[entry]) {
+                            inquirer.prompt([{
+                                type: 'list',
+                                name: 'remove',
+                                message: 'Are you sure?',
+                                choices: [
+                                    {name: 'Yes', value: true},
+                                    {name: 'No', value: false}
+                                ]
+                            }], (answers) => {
+                                if (answers.remove) {
+                                    delete list[entry];
+                                    resolve(list);
+
+                                } else {
+                                    reject('No removal');
+                                }
+                            });
+                        } else {
+                            reject('No matching entry');
+                        }
+                    });
+                })
+                .then((list) => {
+                    return jcrypt.stream(JSON.stringify(list, null, 4), listFile, {
                         gpg: util.getGPGArgs(),
                         file: {
                             flags: 'w',
@@ -75,170 +278,17 @@
                             fd: null,
                             mode: 0o0600
                         }
-                    }, true);
-                })
-                .then(logSuccess)
-                .catch(logError);
-            }
-
-            return (entry) => {
-                if (!entry) {
-                    logError('Must supply an entry name');
-                    return;
-                }
-
-                newEntry = entry;
-                hashedFilename = util.hashFilename(entry);
-
-                // This seems counter-intuitive because the resolve and reject operations
-                // are reversed, but this is b/c the success case is when the file does not
-                // exist (and thus will throw an exception).
-                util.fileExists(`${entryDir}/${hashedFilename}`).then(() => {
-                    logError('Entry already exists');
-                }).catch(() => {
-                    inquirer.prompt([{
-                        type: 'input',
-                        name: 'username',
-                        message: 'Enter username:',
-                        validate: (input) => {
-                            let res = true;
-
-                            if (!input) {
-                                logError('Cannot be blank');
-                                res = false;
-                            }
-
-                            return res;
-                        }
-                    }, {
-                        type: 'list',
-                        name: 'generatePassword',
-                        message: 'Generate diceware password?',
-                        default: false,
-                        choices: [
-                            {name: 'Yes', value: true},
-                            {name: 'No', value: false}
-                        ]
-                    }, {
-                        type: 'password',
-                        name: 'password',
-                        message: 'Enter password:',
-                        when: (answers) => {
-                            return !answers.generatePassword;
-                        }
-                    }], (answers) => {
-                        if (answers.generatePassword) {
-                            generatePassphrase(answers.username);
-                        } else {
-                            makeFileEntry(answers.username, answers.password);
-                        }
-                    });
-                });
-            }
-        })(),
-
-        edit: (() => {
-            function openEditor(file, callback) {
-                let editor = env.EDITOR || 'vim',
-                    // Note: Requiring json will also auto-parse it.
-                    args = require(`./editors/${editor}`);
-
-                // The editor modules will only contain the CLI args
-                // so we need to push on the filename.
-                args.push(file);
-
-                require('child_process').spawn(editor, args, {
-                    stdio: 'inherit'
-                }).on('exit', callback);
-            }
-
-            return (entry) => {
-                let hashedFilename = util.hashFilename(entry),
-                    path = `${entryDir}/${hashedFilename}`;
-
-                util.fileExists(path).then(() => {
-                    jcrypt(path, null, ['--decrypt'])
+                    }, true)
                     .then(() => {
-                        openEditor(path, () => {
-                            // Re-encrypt once done.
-                            jcrypt(path, null, util.getGPGArgs())
-                            .then(() => {
-                                logInfo('Re-encrypting and closing the entry');
-                            })
-                            .catch(logError);
-                        });
+                        logSuccess('Entry has been removed');
                     })
                     .catch(logError);
-                }).catch(logError);
-            };
-        })(),
-
-        generate: () => {
-            console.log(diceware.generate());
-        },
-
-        get: (entry) => {
-            let hashedFilename = util.hashFilename(entry);
-
-            util.fileExists(`${entryDir}/${hashedFilename}`).then((file) => {
-                // Pipe to stdout.
-                jcrypt.stream(file, null, ['--decrypt'])
+                })
                 .catch(logError);
-            }).catch(logError);
-        },
-
-        has: (entry) => {
-            let hashedFilename = util.hashFilename(entry);
-
-            util.fileExists(`${entryDir}/${hashedFilename}`).then(() => {
-                logInfo('Entry exists');
-            }).catch(logError);
-        },
-
-        list: () => {
-            // TODO
-        },
-
-        remove: (() => {
-            function remove(file) {
-                return new Promise((resolve, reject) => {
-                    let shred = require('child_process').spawn('shred', ['--zero', '--remove', file]);
-
-                    shred.on('close', (code) => {
-                        if (code !== 0) {
-                            reject('Something terrible happened!');
-                        } else {
-                            resolve('Entry has been removed');
-                        }
-                    });
-                });
+            } else {
+                libFile.remove(entry);
             }
-
-            return (entry) => {
-                let hashedFilename = util.hashFilename(entry),
-                    path = `${entryDir}/${hashedFilename}`;
-
-                util.fileExists(path).then(() => {
-                    inquirer.prompt([{
-                        type: 'list',
-                        name: 'remove',
-                        message: 'Are you sure?',
-                        choices: [
-                            {name: 'Yes', value: true},
-                            {name: 'No', value: false}
-                        ]
-                    }], (answers) => {
-                        if (answers.remove) {
-                            remove(path)
-                            .then(logSuccess)
-                            .catch(logError);
-                        } else {
-                            logInfo('No removal');
-                        }
-                    });
-                }).catch(logError);
-            };
-        })()
+        }
     };
 
     module.exports = Stymie;
