9b4d25cc0f2a17bf99fb5f9c4a306dd9cef564e8
Benjamin Toll <benjam72@yahoo.com>
2016-05-03 21:42:28 -0400

    Added the `-file` commands

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 [![Build Status](https://travis-ci.org/btoll/stymie.svg?branch=master)](https://travis-ci.org/btoll/stymie)
 
-### About
+## About
 
-### Features
+## Features
 
 - No need to remember another password, `stymie` will use the default GPG key.
 - Create any number of key fields beyond the default username and password.  For example:
@@ -18,12 +18,12 @@ securityAnswer: '1st Avenue'
 - Since everything is stored in `.stymie_d/`, it's easy to port between systems.
 - GPG end-to-end encryption allows `stymie` to be safely versioned.
 
-### Security Features
+## Security Features
 
 - Uses GPG/PGP public-key cryptography to encrypt everything (even configs).
 - Uses OS-level permissions-based access control so only the user can view and list any files created by the user.
 - Cryptographically hashes the key name as the filename when creating encrypted files.
-- Uses the [shred] utility to overwrite any removed file in-place (including a final pass of zeroes to hide the shredding) before unlinking.
+- Uses the [shred] utility to overwrite any removed file in-place (including a final pass of zeroes to hide the shredding) before unlinking. Will default to `rm` when `shred` isn't installed.
 - When using Vim to edit any files (the default), does not leave any swap or backup files during or after editing.
 - Optionally, asks to set `$HISTIGNORE` so `stymie` commands aren't stored in history (See the `postinstall.bash` script for an [example in Bash](scripts/postinstall.bash.example))[1].
 
@@ -31,19 +31,19 @@ securityAnswer: '1st Avenue'
 
 Only Linux and OS X are supported at this time. There are no plans to support Windows.
 
-### Installation
+## Installation
 
 `npm install git+https://github.com/btoll/stymie -g`
 
-### Suggestions
+## Suggestions
 
 - Use `gpg-agent` to save typing.
 - Set `$EDITOR` environment variable to preferred editor. Place editor configs in the `editors/` directory. See the [example for Vim](editors/vim.json).
 
-### Examples
+## Examples
 - Add an encrypted file with the key name `secrets`:
 ```
-stymie add secrets --file
+stymie add-file secrets
 ```
 
 - Create the `example.com` key:
@@ -61,23 +61,40 @@ stymie get example.com --field username
 stymie get example.com --field password | pbcopy
 ```
 
-### Usage
+## Usage
+
+### Commands that operate on password manager entries
 
     Command | Description
-    ------------ | -------------
-    add | Adds a new entry.
-    edit | Edits an entry.
-    generate | Generates a diceware passphrase.
-    get | Retrieves an entry.
-    has | Checks if the entry exists.
-    init | Installs the password file directory and config file.
-    list | List all entries.
-    rm | Deletes an entry.
+    ------- | --------
+    add | Adds a new entry
+    edit | Edits an entry
+    get | Retrieves an entry
+    has | Checks if the entry exists
+    list | List all entries
+    rm | Deletes an entry
+
+### Commands that operate on files
+
+    Command | Description
+    ------- | --------
+    add-file | Adds a new file
+    edit-file | Edits a file
+    get-file | Retrieves a file
+    has-file | Checks if the file exists
+    list-file | List all files (TODO)
+    rm-file | Deletes a file
+
+### Other commands and options
+
+    Command | Description
+    ------- | --------
+    generate | Generates a diceware passphrase
+    init | Installs the password file directory and config file
 
     Option | Description
-    ------------ | -------------
+    ------- | --------
     --field | Gets the specified key value from an entry (only with `get` command).
-    --file | Operate on files.
     -h, --help | Display help.
 
 ## License
--- src/index.js
+++ src/index.js
@@ -17,221 +17,218 @@ let diceware = require('diceware'),
     stymie;
 
 stymie = {
-    add: (key, onFile) => {
+    add: key => {
         if (!key) {
             logError('No key name');
             return;
         }
 
-        if (!onFile) {
-            libKey.make(key);
-        } else {
-            libFile.add(key);
-        }
+        libKey.make(key);
     },
 
-    edit: (key, onFile) => {
-        if (!onFile) {
-            jcrypt(keyFile, null, ['--decrypt'], true)
-            .then((data) => {
-                let list = JSON.parse(data),
-                    item = list[key],
-                    prompts, hasChanged;
+    addFile: key => {
+        if (!key) {
+            logError('No key name');
+            return;
+        }
 
-                if (item) {
-                    hasChanged = {
-                        changed: false
-                    };
+        libFile.add(key);
+    },
 
-                    prompts = [{
-                        type: 'input',
-                        name: 'key',
-                        message: 'Edit key:',
-                        default: key,
-                        validate: libUtil.noDuplicates.bind(null, key, list)
-                    }];
+    edit: key => {
+        jcrypt(keyFile, null, ['--decrypt'], true)
+        .then(data => {
+            let list = JSON.parse(data),
+                item = list[key],
+                prompts, hasChanged;
 
-                    for (let n in item) {
-                        if (item.hasOwnProperty(n)) {
-                            prompts.push({
-                                type: 'input',
-                                name: n,
-                                message: `Edit ${n}:`,
-                                default: item[n],
-                                validate: libUtil.hasChanged.bind(null, hasChanged, n)
-                            });
-                        }
+            if (item) {
+                hasChanged = {
+                    changed: false
+                };
+
+                prompts = [{
+                    type: 'input',
+                    name: 'key',
+                    message: 'Edit key:',
+                    default: key,
+                    validate: libUtil.noDuplicates.bind(null, key, list)
+                }];
+
+                // TODO: Use iterator here.
+                for (let n in item) {
+                    if (item.hasOwnProperty(n)) {
+                        prompts.push({
+                            type: 'input',
+                            name: n,
+                            message: `Edit ${n}:`,
+                            default: item[n],
+                            validate: libUtil.hasChanged.bind(null, hasChanged, n)
+                        });
+                    }
+                }
+
+                inquirer.prompt(prompts, answers => {
+                    let entry = answers.key,
+                        item = list[key];
+
+                    if (entry !== key) {
+                        // Remove old key.
+                        delete list[key];
+
+                        item = list[key] = {};
+
+                        // Note if the key has changed the condition
+                        // below will always pass.
                     }
 
-                    inquirer.prompt(prompts, (answers) => {
-                        let entry = answers.key,
-                            item = list[key];
-
-                        if (entry !== key) {
-                            // Remove old key.
-                            delete list[key];
-
-                            item = list[key] = {};
-
-                            // Note if the key has changed the condition
-                            // below will always pass.
+                    if (hasChanged.changed) {
+                        // TODO: Iterator.
+                        for (let n in answers) {
+                            if (answers.hasOwnProperty(n) && n !== 'key') {
+                                item[n] = answers[n];
+                            }
                         }
 
-                        if (hasChanged.changed) {
-                            for (let n in answers) {
-                                if (answers.hasOwnProperty(n) && n !== 'key') {
-                                    item[n] = answers[n];
-                                }
+                        jcrypt.stream(JSON.stringify(list, null, 4), keyFile, {
+                            gpg: libUtil.getGPGArgs(),
+                            file: {
+                                flags: 'w',
+                                defaultEncoding: 'utf8',
+                                fd: null,
+                                mode: 0o0600
                             }
+                        }, true)
+                        .then(() => {
+                            logSuccess('Key has been updated');
+                        })
+                        .catch(logError);
+                    } else {
+                        logInfo('No change');
+                    }
+                });
+            } else {
+                logInfo('No matching key');
+            }
+        })
+        .catch(logError);
+    },
 
-                            jcrypt.stream(JSON.stringify(list, null, 4), keyFile, {
-                                gpg: libUtil.getGPGArgs(),
-                                file: {
-                                    flags: 'w',
-                                    defaultEncoding: 'utf8',
-                                    fd: null,
-                                    mode: 0o0600
-                                }
-                            }, true)
-                            .then(() => {
-                                logSuccess('Key has been updated');
-                            })
-                            .catch(logError);
+    editFile: key => libFile.edit(key),
+
+    generate: () => log(diceware.generate()),
+
+    get: (key, field) => {
+        jcrypt(keyFile, null, ['--decrypt'], true)
+        .then(data => {
+            let list = JSON.parse(data),
+                item = list[key];
+
+            if (item) {
+                if (!field) {
+                    // TODO: Iterator.
+                    for (let n in item) {
+                        if (item.hasOwnProperty(n) && n !== 'key') {
+                            log(`${n}: ${item[n]}`);
+                        }
+                    }
+                } else {
+                    let f = item[field];
+
+                    if (!f) {
+                        logError('No field found');
+                    } else {
+                        log(f);
+                    }
+                }
+            } else {
+                logInfo('No matching key');
+            }
+        })
+        .catch(logError);
+    },
+
+    getFile: key => libFile.get(key),
+
+    has: key => {
+        jcrypt(keyFile, null, ['--decrypt'], true)
+        .then(data => {
+            logInfo(
+                JSON.parse(data)[key] ?
+                    'Key exists' :
+                    'No matching key'
+            );
+        })
+        .catch(logError);
+    },
+
+    hasFile: key => libFile.has(key),
+
+    list: () => {
+        jcrypt(keyFile, null, ['--decrypt'], true)
+        .then(data => {
+            let keys = Object.keys(JSON.parse(data)),
+                msg;
+
+            msg = !keys.length ?
+                'No installed keys' :
+                `Installed keys: ${keys.sort().join(', ')}`;
+
+            logInfo(msg);
+        });
+    },
+
+    listFile: () => logInfo('Not implemented'),
+
+    rm: key => {
+        jcrypt(keyFile, null, ['--decrypt'], true)
+        .then(data => {
+            let list = JSON.parse(data);
+
+            return new Promise((resolve, reject) => {
+                if (list[key]) {
+                    inquirer.prompt([{
+                        type: 'list',
+                        name: 'rm',
+                        message: 'Are you sure?',
+                        choices: [
+                            {name: 'Yes', value: true},
+                            {name: 'No', value: false}
+                        ],
+                        default: false
+                    }], answers => {
+                        if (answers.rm) {
+                            delete list[key];
+                            resolve(list);
+
                         } else {
-                            logInfo('No change');
+                            reject('No removal');
                         }
                     });
                 } else {
-                    logInfo('No matching key');
+                    reject('No matching key');
                 }
-            })
-            .catch(logError);
-        } else {
-            libFile.edit(key);
-        }
-    },
-
-    generate: () => {
-        log(diceware.generate());
-    },
-
-    get: (key, onFile, field) => {
-        if (!onFile) {
-            jcrypt(keyFile, null, ['--decrypt'], true)
-            .then((data) => {
-                let list = JSON.parse(data),
-                    item = list[key];
-
-                if (item) {
-                    if (!field) {
-                        for (let n in item) {
-                            if (item.hasOwnProperty(n) && n !== 'key') {
-                                log(`${n}: ${item[n]}`);
-                            }
-                        }
-                    } else {
-                        let f = item[field];
-
-                        if (!f) {
-                            logError('No field found');
-                        } else {
-                            log(f);
-                        }
-                    }
-                } else {
-                    logInfo('No matching key');
-                }
-            }).catch(logError);
-        } else {
-            libFile.get(key);
-        }
-    },
-
-    has: (key, onFile) => {
-        if (!onFile) {
-            jcrypt(keyFile, null, ['--decrypt'], true)
-            .then((data) => {
-                logInfo(
-                    JSON.parse(data)[key] ?
-                        'Key exists' :
-                        'No matching key'
-                );
-            }).catch(logError);
-        } else {
-            libFile.has(key);
-        }
-    },
-
-    list: (onFile) => {
-        if (!onFile) {
-            jcrypt(keyFile, null, ['--decrypt'], true)
-            .then((data) => {
-                let keys = Object.keys(JSON.parse(data)),
-                    msg;
-
-                msg = !keys.length ?
-                    'No installed keys' :
-                    `Installed keys: ${keys.sort().join(', ')}`;
-
-                logInfo(msg);
             });
-        } else {
-            // TODO
-            logInfo('Not implemented');
-        }
-    },
-
-    rm: (key, onFile) => {
-        if (!onFile) {
-            jcrypt(keyFile, null, ['--decrypt'], true)
-            .then((data) => {
-                let list = JSON.parse(data);
-
-                return new Promise((resolve, reject) => {
-                    if (list[key]) {
-                        inquirer.prompt([{
-                            type: 'list',
-                            name: 'rm',
-                            message: 'Are you sure?',
-                            choices: [
-                                {name: 'Yes', value: true},
-                                {name: 'No', value: false}
-                            ],
-                            default: false
-                        }], (answers) => {
-                            if (answers.rm) {
-                                delete list[key];
-                                resolve(list);
-
-                            } else {
-                                reject('No removal');
-                            }
-                        });
-                    } else {
-                        reject('No matching key');
-                    }
-                });
-            })
-            .then((list) => {
-                return jcrypt.stream(JSON.stringify(list, null, 4), keyFile, {
-                    gpg: libUtil.getGPGArgs(),
-                    file: {
-                        flags: 'w',
-                        defaultEncoding: 'utf8',
-                        fd: null,
-                        mode: 0o0600
-                    }
-                }, true)
-                .then(() => {
-                    logSuccess('Key has been removed');
-                })
-                .catch(logError);
+        })
+        .then(list => {
+            return jcrypt.stream(JSON.stringify(list, null, 4), keyFile, {
+                gpg: libUtil.getGPGArgs(),
+                file: {
+                    flags: 'w',
+                    defaultEncoding: 'utf8',
+                    fd: null,
+                    mode: 0o0600
+                }
+            }, true)
+            .then(() => {
+                logSuccess('Key has been removed');
             })
             .catch(logError);
-        } else {
-            libFile.rm(key);
-        }
-    }
+        })
+        .catch(logError);
+    },
+
+    rmFile: key => libFile.rm(key)
 };
 
 module.exports = stymie;
