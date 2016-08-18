1c3cfdbec6725f4e076f1bb0a6188df79bc579af
Benjamin Toll <benjam72@yahoo.com>
2016-08-17 20:37:48 -0400

    Begin adding support for creating nested dirs

--- src/file.js
+++ src/file.js
@@ -4,20 +4,21 @@ const cp = require('child_process');
 const fs = require('fs');
 const inquirer = require('inquirer');
 const jcrypt = require('jcrypt');
+const mkdirp = require('mkdirp');
+const path = require('path');
 const util = require('./util');
 const which = require('which');
 
+const env = process.env;
+const filedir = `${env.STYMIE || env.HOME}/.stymie.d/s`;
+const treeFile = `${env.STYMIE || env.HOME}/.stymie.d/f`;
 const logError = util.logError;
 const logInfo = util.logInfo;
 const logSuccess = util.logSuccess;
-const env = process.env;
-const cwd = process.cwd();
-const fileDir = `${env.STYMIE || env.HOME}/.stymie.d/s`;
-const treeFile = `${env.STYMIE || env.HOME}/.stymie.d/f`;
 
 function openEditor(file, callback) {
     const editor = env.EDITOR || 'vim';
-    const editorArgs = require(`${cwd}/editors/${editor}`);
+    const editorArgs = require(`${process.cwd}/editors/${editor}`);
 
     // The editor modules will only contain the CLI args so we need to push on the filename.
     editorArgs.push(file);
@@ -36,43 +37,109 @@ const file = {
 
         const defaultFileOptions = util.getDefaultFileOptions();
         const gpgArgs = util.getGPGArgs();
-        const hashedFilename = util.hashFilename(key);
 
-        // This seems counter-intuitive because the resolve and reject operations
-        // are reversed, but this is b/c the success case is when the file does not
-        // exist (and thus will throw an exception).
-        util.fileExists(`${fileDir}/${hashedFilename}`)
-        .then(() => logError('File already exists'))
-        .catch(() =>
-            jcrypt.encrypt(key, gpgArgs)
-            .then(util.writeFile.bind(null, `${fileDir}/${hashedFilename}`, defaultFileOptions))
-            // Now that the new file has been added we need to record it in the "treefile"
-            // in order to do lookups.
-            // For example:
-            //
-            //      hashedFilename => plaintext
-            //
-            .then(() =>
-                jcrypt.decryptFile(treeFile)
-                .then(data => {
-                    const list = JSON.parse(data);
+        const writeEntry = (list, it) => {
+            let l = list;
 
-                    list[hashedFilename] = key;
+            for (let dir of it) {
+                if (!l[dir]) {
+                    l[dir] = {};
+                }
 
-                    jcrypt.encrypt(JSON.stringify(list, null, 4), gpgArgs)
-                    .then(util.writeFile.bind(null, treeFile, defaultFileOptions));
+                l = l[dir];
+            }
+
+            return l;
+        };
+
+        // Creating an already-existing dir doesn't throw, but maybe clean this up.
+        if (/\/$/.test(key)) {
+            const writeDirsToFile = (list, key) => {
+                const dirs = key.replace(/^\/|\/$/g, '').split('/');
+                writeEntry(list, dirs);
+            };
+
+            mkdirp(`${filedir}/${key}`, err => {
+                if (err) {
+                    logError('Could not create directory');
+                } else {
+                    jcrypt.decryptFile(treeFile)
+                    .then(data => {
+                        let list = JSON.parse(data);
+
+                        list = writeDirsToFile(list, key);
+
+                        jcrypt.encrypt(JSON.stringify(list, null, 4), gpgArgs)
+                        .then(util.writeFile.bind(null, treeFile, defaultFileOptions));
+                    });
+                }
+            });
+        } else {
+            const dirname = path.dirname(key);
+            const basedir = dirname !== '.' ? `${filedir}/${dirname}` : filedir;
+            const hashedFilename = util.hashFilename(path.basename(key));
+
+            const writeKeyToFile = (list, hashedFilename, key) => {
+                let l = list;
+
+                if (dirname !== '.') {
+                    l = writeEntry(l, dirname.split('/'));
+                }
+
+                l[hashedFilename] = path.basename(key);
+            };
+
+            const createEncryptedFile = () =>
+                jcrypt.encrypt(key, gpgArgs)
+                .then(util.writeFile.bind(null, `${basedir}/${hashedFilename}`, defaultFileOptions))
+                // Now that the new file has been added we need to record it in the "treefile"
+                // in order to do lookups.
+                // For example:
+                //
+                //      hashedFilename => plaintext
+                //
+                .then(() =>
+                    jcrypt.decryptFile(treeFile)
+                    .then(data => {
+                        let list = JSON.parse(data);
+
+                        writeKeyToFile(list, hashedFilename, key);
+
+                        jcrypt.encrypt(JSON.stringify(list, null, 4), gpgArgs)
+                        .then(util.writeFile.bind(null, treeFile, defaultFileOptions));
+                    })
+                    .catch(logError)
+                )
+                .then(() => logSuccess('File created successfully'))
+                .catch(logError);
+
+            // This seems counter-intuitive because the resolve and reject operations
+            // are reversed, but this is b/c the success case is when the file does not
+            // exist (and thus will throw an exception).
+            util.fileExists(`${basedir}/${hashedFilename}`)
+            .then(() => logError('File already exists'))
+            .catch(() =>
+                // If the dir already exists then it's safe to create the new file.
+                util.fileExists(basedir)
+                .then(createEncryptedFile)
+                .catch(() => {
+                    // Else, first create the new directory.
+                    mkdirp(basedir, err => {
+                        if (err) {
+                            logError('Could not create directory');
+                        } else {
+                            createEncryptedFile();
+                        }
+                    });
                 })
-                .catch(logError)
-            )
-            .then(() => logSuccess('File created successfully'))
-            .catch(logError)
-        );
+            );
+        }
     },
 
     edit: key => {
         const defaultFileOptions = util.getDefaultFileOptions();
         const hashedFilename = util.hashFilename(key);
-        const path = `${fileDir}/${hashedFilename}`;
+        const path = `${filedir}/${hashedFilename}`;
 
         util.fileExists(path).then(() =>
             jcrypt.decryptToFile(path)
@@ -92,7 +159,7 @@ const file = {
     get: key => {
         const hashedFilename = util.hashFilename(key);
 
-        util.fileExists(`${fileDir}/${hashedFilename}`)
+        util.fileExists(`${filedir}/${hashedFilename}`)
         .then(file =>
             // Pipe to stdout.
             jcrypt.stream(file, null, ['--decrypt'])
@@ -104,7 +171,7 @@ const file = {
     has: key => {
         const hashedFilename = util.hashFilename(key);
 
-        util.fileExists(`${fileDir}/${hashedFilename}`)
+        util.fileExists(`${filedir}/${hashedFilename}`)
         .then(() => logInfo('File exists'))
         .catch(logError);
     },
@@ -114,11 +181,18 @@ const file = {
         .then(data => {
             let list = JSON.parse(data);
 
-            fs.readdir(fileDir, (err, files) => {
+            fs.readdir(filedir, (err, files) => {
                 if (!files.length) {
                     util.logInfo('No files');
                 } else {
-                    files.forEach(file => logInfo(list[file]));
+//                    files.forEach(file => logInfo(list[file]));
+                    files.forEach(file => {
+                        if (typeof list[file] === 'object') {
+                            logInfo(`${file}/`);
+                        } else {
+                            logInfo(list[file]);
+                        }
+                    });
                 }
             });
         })
@@ -150,7 +224,7 @@ const file = {
 
         return key => {
             const hashedFilename = util.hashFilename(key);
-            const path = `${fileDir}/${hashedFilename}`;
+            const path = `${filedir}/${hashedFilename}`;
 
             util.fileExists(path)
             .then(() =>
